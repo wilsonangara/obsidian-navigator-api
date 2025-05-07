3983684ec71cfd0e820241f3888e21fad1f5df4e
Wilson Angara <wilsonangara@gmail.com>
2025-05-07 22:43:28 +0700

    feat: implement fastify routing and healthcheck endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
 import { Plugin } from "obsidian";
+import Fastify, { FastifyInstance } from "fastify";
 
 export default class ObsidianNavigatorAPI extends Plugin {
+  public port: number = 27124;
+  private fastify: FastifyInstance | undefined;
+
   async onload(): Promise<void> {
-    // load settings here
+    this.fastify = Fastify();
+
+    // health endpoint
+    this.fastify.get(
+      "/health",
+      { schema: { response: { 200: { message: { type: "string" } } } } },
+      async (_, reply) => {
+        reply.code(200).send({
+          message: "ok",
+        });
+      },
+    );
+
+    this.fastify.listen({ port: this.port }, (err, address) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      console.log(`listening to: ${address}`);
+    });
   }
 
   onunload(): void {
-    // clean up here
+    if (this.fastify) {
+      console.log(`Shutting down server on port ${this.port}`);
+      this.fastify.close();
+    }
   }
 }
